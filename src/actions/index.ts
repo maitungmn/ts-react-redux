267058ee4ac365fbec1a9fcb4a5622f41dfd7fbf
Mai Tung <goingdownfr@gmail.com>
2020-05-16 15:59:36 +0700

    wrapup

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,27 +1,2 @@
-import { ActionTypes } from './types';
-import axios from 'axios';
-import { Dispatch } from 'redux';
-
-export interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-}
-
-export interface FetchTodosAction {
-  type: ActionTypes.fetchTodos;
-  payload: Todo[];
-}
-
-const url = 'https://jsonplaceholder.typicode.com/todos';
-
-export const fetchTodos = () => {
-  return async (dispatch: Dispatch) => {
-    const res = await axios.get<Todo[]>(url);
-
-    dispatch<FetchTodosAction>({
-      type: ActionTypes.fetchTodos,
-      payload: res.data,
-    });
-  };
-};
+export * from './todos';
+export * from './types';
